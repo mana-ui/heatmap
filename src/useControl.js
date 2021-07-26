858c82942852afaa8b8b42c8736432065b9b4f2c
xiejin <xiejin@homay.com.cn>
2021-07-26 20:20:53 +0800

    🐛 修复一个兼容性问题

--- src/useControl.js
+++ src/useControl.js
@@ -1,23 +1,23 @@
 import { useRef, useEffect, useContext } from "react";
 import observer from "./Observer";
-import Context from './Context'
+import Context from "./Context";
 import { interpolateNumber } from "d3-interpolate";
 import throttle from "lodash.throttle";
 
 export default function useControl({ minRef, max, maxRef, min }) {
   const minPosRef = useRef(max);
-  const maxPosRef = useRef(min)
+  const maxPosRef = useRef(min);
   const draggingRef = useRef(null);
-  const {min: minValue, max: maxValue} = useContext(Context)
+  const { min: minValue, max: maxValue } = useContext(Context);
   useEffect(() => {
-    minRef.current.move(max)
-    maxRef.current.move(min)
-    rangeRef.current = [minValue, maxValue]
-    minPosRef.current = max
-    maxPosRef.current = min
-  }, [minValue, maxValue])
-  const rangeRef = useRef([minValue, maxValue])
-  const interpolate = interpolateNumber(minValue, maxValue)
+    minRef.current.move(max);
+    maxRef.current.move(min);
+    rangeRef.current = [minValue, maxValue];
+    minPosRef.current = max;
+    maxPosRef.current = min;
+  }, [minValue, maxValue]);
+  const rangeRef = useRef([minValue, maxValue]);
+  const interpolate = interpolateNumber(minValue, maxValue);
   const dragEnd = () => {
     if (draggingRef.current) {
       draggingRef.current.posRef.current = draggingRef.current.pos;
@@ -25,36 +25,53 @@ export default function useControl({ minRef, max, maxRef, min }) {
     }
   };
   const emit = throttle(() => {
-    observer.emit('change', rangeRef.current)
-  }, 10)
+    observer.emit("change", rangeRef.current);
+  }, 10);
 
   const handlers = {
-    mousedown({target, clientY}) {
+    mousedown({ target, clientY }) {
       if (target === minRef.current.elem) {
-          draggingRef.current = { y: clientY, pos: minPosRef.current, posRef: minPosRef, ref: minRef, idx: 0 };
+        draggingRef.current = {
+          y: clientY,
+          pos: minPosRef.current,
+          posRef: minPosRef,
+          ref: minRef,
+          idx: 0,
+        };
       } else if (target === maxRef.current.elem) {
-          draggingRef.current = { y: clientY, pos: maxPosRef.current, posRef: maxPosRef, ref: maxRef, idx: 1 };
+        draggingRef.current = {
+          y: clientY,
+          pos: maxPosRef.current,
+          posRef: maxPosRef,
+          ref: maxRef,
+          idx: 1,
+        };
       }
     },
     mousemove(evt) {
       evt.preventDefault();
       if (draggingRef.current) {
         const delta = evt.clientY - draggingRef.current.y;
-        const pos = Math.max(Math.min(delta + draggingRef.current.posRef.current, max), min);
+        const pos = Math.max(
+          Math.min(delta + draggingRef.current.posRef.current, max),
+          min
+        );
         draggingRef.current.pos = pos;
         draggingRef.current.ref.current.move(pos);
-        const d = (max - pos)/(max - min)
-        rangeRef.current[draggingRef.current.idx] = interpolate(d)
-        emit()
+        const d = (max - pos) / (max - min);
+        rangeRef.current[draggingRef.current.idx] = interpolate(d);
+        emit();
       }
     },
     mouseup: dragEnd,
     mouseleave: dragEnd,
-  }
+  };
   useEffect(() =>
-    observer.listen('event', event => {
-        const {type } = event
-        handlers[type]?.(event)
+    observer.listen("event", (event) => {
+      const { type } = event;
+      if (typeof handlers[type] === "function") {
+        handlers[type](event);
+      }
     })
   );
 }
